9dfd4fc6ea11a260d2d86925dbabf3d53d76bef2
Sitt Guruvanich <aekazitt@gmail.com>
2024-03-26 00:20:30 +0700

    Refactor "robin" to "bitvm"

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use pyo3::prelude::*;
 use pyo3::{create_exception, exceptions::PyException};
 
-create_exception!(robin, VirtualMachineError, PyException);
+create_exception!(bitvm, VirtualMachineError, PyException);
 
 mod instruction;
 mod merkle_path;
@@ -11,7 +11,7 @@ mod snapshot;
 mod vm;
 
 #[pymodule]
-fn robin(_py: Python, m: &PyModule) -> PyResult<()> {
+fn bitvm(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<instruction::Instruction>()?;
     m.add_class::<merkle_path::MerklePath>()?;
     m.add_class::<snapshot::Snapshot>()?;
