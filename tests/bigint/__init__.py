0c62968d4050b8ddcbc1fb5a891483bea42a7e3b
Sitt Guruvanich <aekazitt@gmail.com>
2024-04-12 13:37:07 +0700

    Create skeletal for Bigint add

---
+++ tests/bigint/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.8
+# coding:utf-8
+# Copyright (C) 2024 All rights reserved.
+# FILENAME:    ~~/tests/bigint/__init__.py
+# VERSION: 	   0.1.0
+# CREATED: 	   2024-04-12 13:32
+# AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+"""
+Tests fot Bigint implementation
+"""
+
+### Standard packages ###
+from random import random, seed
+
+def test_add() -> None:
+  ...
